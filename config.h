6675017e45ac924b3858e604a84a2964c4fbc560
Johnathan Davis <davisjc@users.noreply.github.com>
2016-02-12 20:48:18 -0800

    Apply hbw's Aurora color scheme

--- config.h
+++ config.h
@@ -70,31 +70,34 @@ static unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* JCD - Apply hbw's Aurora color scheme. */
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000",
+	"#dd0000",
+	"#00bb00",
+	"#bbbb00",
+	"#1e21fe",
+	"#bb00bb",
+	"#00bbbb",
+	"#bbbbbb",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#777777",
+	"#ff5555",
+	"#55ff55",
+	"#ffff55",
+	"#5482ff",
+	"#ff55ff",
+	"#55ffff",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
-	"#555555",
+	"#002b36",  /* Aurora background */
+	"#bcbcbc",  /* Aurora foreground */
+	/* JCD - Finish applying hbw's Aurora color scheme. */
 };
 
 
@@ -102,8 +105,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-static unsigned int defaultfg = 7;
-static unsigned int defaultbg = 0;
+static unsigned int defaultfg = 258; /* JCD - hbw's Aurora colors. */
+static unsigned int defaultbg = 257; /* JCD - hbw's Aurora colors. */
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
